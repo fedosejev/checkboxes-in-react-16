47c68342f8059593d80da63648880227a1112fa0
Artemij Fedosejev <artemij.fedosejev@gmail.com>
2019-01-06 04:38:08 +0000

    Updates

---
+++ precache-manifest.5f643cfa0824a6f72c68dcf6cd6b3af3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7420a4192e05938b3195",
+    "url": "/checkboxes-in-react-16/static/js/runtime~main.7420a419.js"
+  },
+  {
+    "revision": "6057b94b9c1e894d6b17",
+    "url": "/checkboxes-in-react-16/static/js/main.6057b94b.chunk.js"
+  },
+  {
+    "revision": "91caeaab80ad42c0bcc5",
+    "url": "/checkboxes-in-react-16/static/js/1.91caeaab.chunk.js"
+  },
+  {
+    "revision": "14b939cef66ccc3a8330731ff55f136a",
+    "url": "/checkboxes-in-react-16/index.html"
+  }
+];
